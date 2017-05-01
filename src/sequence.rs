4393c4a2450def2f200cf35a1638116477d51345
Benjamin Allen <benjamin.daniel.allen@gmail.com>
2017-05-01 18:03:50 -0400

    codon

--- src/sequence.rs
+++ src/sequence.rs
@@ -1,10 +1,34 @@
 use nucleotide::NucleotideLike;
 
+#[derive(Debug)]
 pub struct Sequence<N> where N: NucleotideLike {
     data: Vec<N>,
 }
 
-impl<N> Sequence<N> where N: NucleotideLike<N=N> {
+#[derive(Debug)]
+pub struct Codon<'a, N> where N: NucleotideLike + 'a {
+    data: &'a [N],
+}
+
+/*
+Compare two Codons
+*/
+impl<'a, N> PartialEq for Codon<'a, N> where N: NucleotideLike + PartialEq + 'a {
+    fn eq(&self, other: &Codon<'a, N>) -> bool {
+        self.data == other.data
+    }
+}
+
+/*
+Compare LHS=Sequence with RHS=Codon
+*/
+impl<'a, N> PartialEq<Codon<'a,N>> for Sequence<N> where N: NucleotideLike + PartialEq + 'a {
+    fn eq(&self, other: &Codon<'a, N>) -> bool {
+        self.data == other.data
+    }
+}
+
+impl<N> Sequence<N> where N: NucleotideLike<N=N> + PartialEq {
     pub fn new() -> Sequence<N> {
         Sequence::<N>{data: Vec::new()}
     }
@@ -28,6 +52,12 @@ impl<N> Sequence<N> where N: NucleotideLike<N=N> {
             rc.data.push(nt.complement());
         }
         rc
+    }
+    pub fn codon(&self, index: usize) -> Result<Codon<N>, String> {
+        if index+3 <= self.data.len()
+            { Ok(Codon::<N> { data: &self.data[index .. index+3] } ) }
+        else
+            { Err(format!("Codon index out of bounds")) }
     }
 }
 
@@ -86,6 +116,33 @@ mod tests {
             Ok(dnts) => assert_eq!("YMRNTAC",
                                    dnts.reverse_complement().to_string()),
             Err(e) => panic!(e)
+        }
+    }
+
+    #[test]
+    fn codon() {
+        let input_result = Sequence::<Nucleotide>::from_str("GTAAAAC");
+        let codon1_result = Sequence::<Nucleotide>::from_str("GTA");
+        let codon2_result = Sequence::<Nucleotide>::from_str("TAA");
+        match (input_result,codon1_result,codon2_result) {
+            (Ok(input),Ok(codon1),Ok(codon2)) => {
+                let test1_result = input.codon(0); // should be "GTA"
+                let test2_result = input.codon(1); // should be "TAA"
+                match (test1_result,test2_result) {
+                    (Ok(test1),Ok(test2)) => {
+                        assert_eq!(codon1,test1);
+                        assert_eq!(codon2,test2);
+                    }
+                    _ => panic!("bad sequence input")
+                }
+                // trying to take a codon out of bounds should return Err
+                let test3_result = input.codon(5);
+                match test3_result {
+                    Ok(_) => panic!("did not error for out-of-bounds codon"),
+                    Err(_) => ()
+                }
+            }
+            _ => panic!("bad sequence input")
         }
     }
 }
